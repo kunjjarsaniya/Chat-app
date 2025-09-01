1273f37e0559a09d8b02cec991ba06ace81a55a9
kunjjarsaniya <kunjjarsaniya07@gmail.com>
2025-09-01 17:22:51 +0530

    uploding port in backend

--- server/server.js
+++ server/server.js
@@ -62,7 +62,7 @@ app.get("*", (req, res) => {
 })
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
